d80f777e9b679c7d6ff7a3ccf09535a0e89409fb
artfront <krivonosart19@gmail.com>
2019-03-21 09:39:55 +0300

    solve the task in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	let numOfCoins = {};
+	const numOfH = Math.floor(currency/50);
+	const remOfDivH=currency%50;
+	const numOfQ = Math.floor(remOfDivH/25);
+	const remOfDivQ = remOfDivH%25;
+	const numOfD = Math.floor(remOfDivQ/10);
+	const remOfDivD = remOfDivQ%10;
+	const numOfN = Math.floor(remOfDivD/5);
+	const numOfP = remOfDivD%5;
+if (currency<=0) return {};
+if(currency>10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+if (numOfH>=1) numOfCoins.H = numOfH;
+if (numOfQ>=1) numOfCoins.Q = numOfQ;
+if (numOfD>=1) numOfCoins.D = numOfD;
+if (numOfN>=1) numOfCoins.N = numOfN;
+if (numOfP>=1) numOfCoins.P = numOfP;
+return numOfCoins;
 }
